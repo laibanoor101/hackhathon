e7bf2ef067f147101c99c5d0a7e43ff69a60aeac
laibanoor101 <164699732+laibanoor101@users.noreply.github.com>
2024-12-08 16:51:01 -0800

    initial commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import logo from "@/app/picture/logo.jpeg";
+import Link from "next/link";
+import { MdPersonOutline } from "react-icons/md"
+import { CiSearch } from "react-icons/ci";
+import { GoHeart } from "react-icons/go";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+
+export default function Header() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="logo ">
+
+
+        <Image src={logo} width={185} height={41} alt="Picture of the author" />
+        </div>
+
+        <div className="navbtns">
+        <ul className="head">
+          <Link href={"/"}>
+            <li>Home</li>
+          </Link>
+<Link href={"/shop"}>
+            <li>Shop</li>
+          </Link>
+          <Link href={"/blog"}>
+            <li>Blog</li>
+          </Link>
+          <Link href={"/contact"}>
+            <li>Contact</li>
+          </Link>
+        </ul>
+        </div>
+
+        <div className="icons">
+
+        </div>
+        <MdPersonOutline />
+        <CiSearch />
+        <GoHeart />
+        <AiOutlineShoppingCart />
+
+
+      </div>
+    </>
+  );
+}
---
+++ src/app/components/Section.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import React from "react";
+
+const Section = () => {
+  return (
+    <div>
+      {/* Section */}
+      <div className="absolute top-[859px] left-[131px] w-[1183px] h-[530px] bg-white">
+        {/* First div for Heading and Paragraph */}
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Browse The page</h1>
+          <p className="mt-2 text-gray-600">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+          <br />
+        </div>
+
+        {/* Second div for Images with Names */}
+        <div className="flex space-x-4 justify-center mt-4 rounded-lg">
+          {/* First Image with Name */}
+          <div className="text-center">
+            <Image
+              src="/dining.jpeg"
+              width={381}
+              height={480}
+              alt="Dining"
+            />
+            <h2 className="mt-2 ">Dining</h2>
+          </div>
+
+          {/* Second Image with Name */}
+          <div className="text-center">
+            <Image
+              src="/living.jpeg"
+              width={381}
+              height={480}
+              alt="Living"
+            />
+            <p className="mt-2">Living</p>
+          </div>
+
+          {/* Third Image with Name */}
+          <div className="text-center">
+            <Image
+              src="/bedroom.jpeg"
+              width={381}
+              height={480}
+              alt="Bedroom"
+            />
+            <p className="mt-2">Bedroom</p>
+          </div>
+        </div>
+      </div>
+</div>
+  );
+};
+
+export default Section;
